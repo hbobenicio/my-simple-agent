4cb895bc9ec3aa75d278747bab27733528870f98
hbobenicio <hbobenicio@gmail.com>
2026-06-28 13:53:30 -0300

    add LLM_API_KEY config

--- src/main/java/mysimpleagent/config/Config.java
+++ src/main/java/mysimpleagent/config/Config.java
@@ -16,33 +16,44 @@ public class Config {
 
     private String llmSystemPrompt;
 
+    private String llmApiKey;
+
     public static Config loadFromEnv() {
         logger.atDebug().log("loading configs from env..");
 
         var cfg = new Config();
-        cfg.llmBaseUrl = Optional.ofNullable(System.getenv("LLM_BASE_URL"))
-                .map(String::trim)
-                .orElse("http://localhost:12434/v1");
-        logger.atDebug()
-                .addKeyValue("LLM_BASE_URL", cfg.llmBaseUrl)
-                .log("value loaded");
 
-        String maybeModelName = System.getenv("LLM_MODEL_NAME");
-        if (maybeModelName != null) {
-            cfg.llmModelName = maybeModelName.trim();
-            logger.atDebug()
-                    .addKeyValue("LLM_MODEL_NAME", cfg.llmModelName)
-                    .log("value loaded");
-        }
+        cfg.llmBaseUrl = load("LLM_BASE_URL", "http://localhost:12434/v1", false);
+        cfg.llmModelName = load("LLM_MODEL_NAME", null, false);
+        cfg.llmApiKey = load("LLM_API_KEY", null, true);
 
-        //TODO This is not a config. It's a resource that should be loaded at startup and should be
-        //     saved in the global application scope (or be restricted to the LLMService)
+        //TODO This is not a config. It's an application scoped resource that should be loaded at startup
+        //     Move this to another place
         cfg.llmSystemPrompt = ResourceUtils.loadResourceAsString("/system.prompt.txt");
-        logger.atDebug()
-                .addKeyValue("systemPromptSize", cfg.llmSystemPrompt.length())
-                .log("system prompt loaded");
 
         return cfg;
+    }
+
+    private static String load(String key, String defaultValue, boolean redacted) {
+        Optional<String> maybeValue = Optional.ofNullable(System.getProperty(key))
+                .filter(s -> !s.isBlank())
+                .map(String::trim);
+
+        maybeValue.ifPresent(value -> logValueLoadedEvent(key, value, redacted));
+
+        return maybeValue.orElse(defaultValue);
+    }
+
+    private static void logValueLoadedEvent(String key, String value, boolean redacted) {
+        var logBuilder = logger.atDebug().addKeyValue("key", key);
+
+        if (redacted) {
+            logBuilder = logBuilder.addKeyValue("value", "<REDACTED>");
+        } else {
+            logBuilder = logBuilder.addKeyValue("value", value);
+        }
+
+        logBuilder.log("value loaded");
     }
 
     public String getLlmBaseUrl() {
@@ -59,5 +70,9 @@ public class Config {
 
     public String getLlmSystemPrompt() {
         return llmSystemPrompt;
+    }
+
+    public Optional<String> getLlmApiKey() {
+        return Optional.ofNullable(llmApiKey);
     }
 }
